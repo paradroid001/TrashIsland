f065d1d3aa1600dcaf36799e092977baae75f47d
Patrickl33 <patrick.a.lee22@gmail.com>
2023-03-24 19:01:25 +0800

    added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y + 5.5f, player.position.z - 7f);
+    }
+}
