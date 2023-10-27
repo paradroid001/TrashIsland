9db984beeeb51790dfda4319c8c2042025944b81
FoodAbuse <106326154+FoodAbuse@users.noreply.github.com>
2023-10-28 02:58:31 +0800

    Washer Minigame Barebones Functionality
    Function for drawing over textures
    
    Function for gameplay loop
    
    lack of connected function between the two

---
+++ Assets/Dev/Washer Dev/Scripts/WasherManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasherManager : MonoBehaviour
+{
+    enum GameState {Tutorial, Play, End};
+
+    public Transform dirtyInventory;
+    public Transform cleanInventory;
+    public Transform washingInventory;
+
+    public Transform spawnPosition;
+
+    public List<GameObject> liveItemsList;
+    public GameObject[] liveItemsArray;
+    public GameObject currentObject = null;
+    public int currentCount;
+
+    public bool isCleaning = false;
+    public float itemCleanliness;
+    public float transitionTime = 2f;
+
+    public bool pregame = true;
+
+    public int itemCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SetupGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (isCleaning)
+        { 
+            if (Input.GetKey(KeyCode.E))
+            {
+                //Debug.Log("cleaning");
+                itemCleanliness += Time.deltaTime;
+            }
+
+            if (itemCleanliness >= 5f)
+            {
+                Debug.Log("cleaning complete");
+
+                // Moves object position from cleaning to cleaned inventory
+                currentObject.transform.position = cleanInventory.position;
+
+                // Sets cleaned inventory as parent
+                currentObject.transform.SetParent(cleanInventory);
+
+                isCleaning = false;
+
+                StartCoroutine(itemTransition());
+            }
+        }
+
+        if (pregame && Input.GetKeyDown(KeyCode.Space))
+        {
+            pregame = false;
+            SetupGame();
+        }
+
+    }
+
+    void SetupGame()
+    {
+        
+        
+        foreach(Transform child in dirtyInventory)
+        {
+             liveItemsList.Add(child.gameObject);
+        }
+
+        liveItemsArray = liveItemsList.ToArray();
+        itemCount = liveItemsList.Count;
+        liveItemsList = null;
+        //Debug.Log(itemCount + " items were found");
+
+        currentCount = 1;
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        currentObject = liveItemsArray[currentCount - 1];
+
+        CleaningTime();
+
+    }
+
+    void CleaningTime()
+    {
+        itemCleanliness = 0;
+
+        currentObject.transform.position = spawnPosition.position;
+        currentObject.transform.SetParent(washingInventory);
+
+        isCleaning = true;
+    }
+
+    public IEnumerator itemTransition()
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+        if (currentCount >= itemCount)
+        {
+            WinGame();
+        }
+        else
+        {
+            currentCount++;
+            StartGame();
+        }
+
+    }
+
+    void WinGame()
+    {
+        if (itemCount >= liveItemsArray.Length)
+        {
+            //Win
+        }
+    }
+}
