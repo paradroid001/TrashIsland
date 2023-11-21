99850ff4c5e6e0287d6be98c9cf52692f7b5cd2a
FoodAbuse <106326154+FoodAbuse@users.noreply.github.com>
2023-11-22 02:45:35 +0800

    Environment Development + Interior Textures

---
+++ Assets/SpriteStorage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteStorage : MonoBehaviour
+{
+    public Texture2D grumpy;
+    public Texture2D incorrect;
+}
+
