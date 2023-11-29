1f917a617f2a9adafb7347d5ac6d08672d965101
Patrickl33 <101612940+Patrickl33@users.noreply.github.com>
2023-11-29 13:14:34 +0800

    Dialogue emotion sprites output

--- Assets/Dev/Demo - Main Level Stuff/Scripts/DemoDialoguePortraitsStorage.cs
+++ Assets/Dev/Demo - Main Level Stuff/Scripts/DemoDialoguePortraitsStorage.cs
@@ -18,48 +18,5 @@ public class DialoguePortraits
 }
 public class DialoguePortraitPasser : Object
 {
-    [YarnCommand("SetCharacter")]
-    public Sprite PassSprite(string characterName)
-    {
-        Sprite spriteReturned = null;
-        for (int i = 0; i < DemoManager.instance.dialoguePortraits.portraits.Count; i++)
-        {
-            if (DemoManager.instance.dialoguePortraits.portraits[i].charName == characterName)
-            {
-                spriteReturned = DemoManager.instance.dialoguePortraits.portraits[i].emotionSprites[0];
-            }
-            else
-            {
-                Debug.Log("Name Not Found");
-            }
-        }
-        return spriteReturned;
-    }
-    [YarnCommand("SetCharacterEmotion")]
-    public Sprite PassSprite(string characterName, string emotionName)
-    {
-        Sprite spriteReturned = null;
-        for(int i = 0; i < DemoManager.instance.dialoguePortraits.portraits.Count; i++)
-        {
-            if (DemoManager.instance.dialoguePortraits.portraits[i].charName == characterName)
-            {
-                if (DemoManager.instance.dialoguePortraits.portraits[i].emotionNames.Contains(emotionName))
-                {
-                    int spriteNo = DemoManager.instance.dialoguePortraits.portraits[i].emotionNames.IndexOf(emotionName);
-                    spriteReturned = DemoManager.instance.dialoguePortraits.portraits[i].emotionSprites[spriteNo];
-                }
-                else
-                {
-                    spriteReturned = DemoManager.instance.dialoguePortraits.portraits[i].emotionSprites[0];
-                    Debug.Log("Emotion Name Not Found");
-                }
-            }
-            else
-            {
-                Debug.Log("Name Not Found");
-            }
-        }
-        
-        return spriteReturned;
-    }
+    
 }
